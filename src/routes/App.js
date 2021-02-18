b1ddee855a87e6a14e5f928ff88675b34a80266a
Carlos Benitez <carlosma155@hotmail.com>
2021-02-17 20:45:27 -0500

    Configuracion de webpack

---
+++ src/routes/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const App = () => {
+    return (
+        <h1>
+            Hola mundo
+        </h1>
+    )
+}
+
+export default App
